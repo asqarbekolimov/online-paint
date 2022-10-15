5d81568ba27ab010da21a10e4ace51ceb086766c
asqarbekolimov <mr.coderx.pro@gmail.com>
2022-10-15 14:36:34 +0500

    Rectengle draw

--- js/app.js
+++ js/app.js
@@ -1,27 +1,59 @@
-const canvas = document.querySelector('canvas')
-isDrawing = false
-brushWidth = 5
+const canvas = document.querySelector('canvas'),
+  toolBtns = document.querySelectorAll('.tool')
+
+let ctx = canvas.getContext('2d'),
+  isDrawing = false,
+  brushWidth = 5,
+  selectedTool = 'brush',
+  prevMouseX,
+  prevMouseY,
+  snapshot
 
 window.addEventListener('load', () => {
   canvas.width = canvas.offsetWidth
   canvas.height = canvas.offsetHeight
 })
 
-let ctx = canvas.getContext('2d')
-
 
 const startDrawing = e => {
   isDrawing = true
+  prevMouseX = e.offsetX
+  prevMouseY = e.offsetY
   ctx.beginPath()
   ctx.lineWidth = brushWidth
+  snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height)
+  console.log(snapshot);
+}
+
+const drawRectangle = e => {
+  ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY)
 }
 
 const drawing = e => {
   if (!isDrawing) return
-  ctx.lineTo(e.offsetX, e.offsetY)
+  ctx.putImageData(snapshot, 0, 0)
+  switch (selectedTool) {
+    case 'brush':
+      ctx.lineTo(e.offsetX, e.offsetY)
+      ctx.stroke()
+      break
+    case 'rectangle':
+      drawRectangle(e)
+      break
+    default:
+      break;
+  }
 
-  ctx.stroke()
 }
+
+toolBtns.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    document.querySelector('.options .active').classList.remove('active')
+    btn.classList.add('active')
+    selectedTool = btn.id
+    console.log(`Selectedd tool ${selectedTool}`);
+  })
+})
 
 const stopDrawing = e => {
   isDrawing = false
