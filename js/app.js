90f4eb2482f2bd38661085f4ade3152e59e926b7
asqarbekolimov <mr.coderx.pro@gmail.com>
2022-10-15 14:45:30 +0500

    Fill draw

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector('canvas'),
-  toolBtns = document.querySelectorAll('.tool')
+  toolBtns = document.querySelectorAll('.tool'),
+  fillColor = document.querySelector('#fill-color')
 
 let ctx = canvas.getContext('2d'),
   isDrawing = false,
@@ -26,7 +27,11 @@ const startDrawing = e => {
 }
 
 const drawRectangle = e => {
-  ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY)
+  if (!fillColor.checked) {
+    return ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY)
+  }
+
+  ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY)
 }
 
 const drawing = e => {
